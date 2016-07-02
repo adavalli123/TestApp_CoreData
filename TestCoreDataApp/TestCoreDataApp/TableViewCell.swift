e669fbec4fa810862772041770c4fc631b4faacc
Srikanth Adavalli <sadavalli@beachbody.com>
2016-07-01 18:19:24 -0700

    Added Core Data

---
+++ TestCoreDataApp/TestCoreDataApp/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  TestCoreDataApp
+//
+//  Created by Srikanth Adavalli on 7/1/16.
+//  Copyright © 2016 Srikanth Adavalli. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+}
---
+++ TestCoreDataApp/TestCoreDataApp/TableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  TestCoreDataApp
+//
+//  Created by Srikanth Adavalli on 6/30/16.
+//  Copyright © 2016 Srikanth Adavalli. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TableViewController: UIViewController, UITableViewDelegate {
+    
+    // IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //setting variables
+    var people = [NSManagedObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\"The List\""
+        tableView.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchLog()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! TableViewCell
+        
+        let person = people[indexPath.row]
+        cell.nameLabel.text = person.valueForKey("name") as? String
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            let removingObject = people[indexPath.row]
+            let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+            let managedContext = appDelegate?.managedObjectContext
+            
+            managedContext?.deleteObject(removingObject)
+            people.removeAtIndex(indexPath.row)
+            
+            do {
+                try managedContext!.save()
+            }
+            catch {
+                print("Error")
+            }
+            
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+            
+            //            self.saveName(nil)
+            tableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    @IBAction func addNameBarButton(sender: AnyObject) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default,
+                                       handler: { (action:UIAlertAction) -> Void in
+                                        
+                                        let textField = alert.textFields!.first
+                                        self.saveName(textField!.text!)
+                                        self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        alert.view.setNeedsLayout()
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
+        
+    }
+    
+    func saveName(name: String?) {
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        let managedContext = appDelegate?.managedObjectContext
+        
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext!)
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        do {
+            try managedContext!.save()
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    func fetchLog() {
+        let appDelegate =
+            UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        do {
+            let results =
+                try managedContext.executeFetchRequest(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+}
+
